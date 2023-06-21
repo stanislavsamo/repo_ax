78e5deaacd71752a906f982d14e30bdb10137ec8
stanislavsamo <f11samokhin@gmail.com>
2023-06-21 11:04:17 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const viewer_1 = new PhotoSphereViewer.Viewer({
+    container: document.querySelector('#viewer_1'),
+    panorama: 'src/360d/2.png',
+});
+
+const viewer_2 = new PhotoSphereViewer.Viewer({
+    container: document.querySelector('#viewer_2'),
+    panorama: 'src/360d/1.jpg',
+});
+
+const viewer_3 = new PhotoSphereViewer.Viewer({
+    container: document.querySelector('#viewer_3'),
+    panorama: 'src/360d/3.jpg',
+});
+
+const viewer_4 = new PhotoSphereViewer.Viewer({
+    container: document.querySelector('#viewer_4'),
+    panorama: 'src/360d/4.jpg',
+});
